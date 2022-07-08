2b2cb586bb554860263a712fde18b15bbb4dd364
woolleygrammoth <woolleygrammoth@users.noreply.github.com>
2022-07-07 22:56:34 -0700

    troubleshooting: make downloading more flexible

--- .ipynb_checkpoints/downloads-checkpoint.py
+++ .ipynb_checkpoints/downloads-checkpoint.py
@@ -5,32 +5,30 @@ import os
 import time
 import datetime
 
-start = '06/02/2022'
-end = '06/02/2022'
-dates = generate_dates(start, end)
+start = '04/10/2018'
+end = '04/10/2018'
 
-for date in dates: 
-    log = './scraped-data/download_logs.txt'
-    with open(log, 'r') as f: 
-        contents = f.read()
-    # if date not already logged
-    if date not in contents: 
+for date in generate_dates(start, end): 
+    log = 'scraped-data/download_logs.txt'
+#     with open(log, 'r') as f: 
+#         contents = f.read()
+#     # if date not already logged
+#     if date not in contents: 
         # download net demand for date into daily folder
-        download_net_demand(date)
-        time.sleep(2) #give the download time
+    download_net_demand(date)
+    time.sleep(2) #give the download time
         # open day_csv in daily folder & set its index
-        file_name = file_name_from_date(date)
-        df = get_df_new(file_name, path='./scraped-data/daily/')
-        day_df = set_dt_index_new(df, date)
-        # open master_csv in ./scraped-data with index_col="Timestamp"
-        master_df = pd.read_csv('./scraped-data/caiso-net-demand.csv', index_col="Timestamp")
+    file_name = file_name_from_date(date)
+    df = get_df_old(file_name, path='./scraped-data/daily/')
+    today_df = set_dt_index_old(df, date)
+        # master_df = pd.read_csv('./scraped-data/caiso-net-demand.csv', index_col="Timestamp")
         # concatenate daily csv to master csv 
-        master_df = pd.concat([master_df, day_df])
+        # master_df = pd.concat([master_df, day_df])
         # master_df.drop_duplicates(subset='Timestamp', inplace=True)
         # overwrite the master csv
-        master_df.to_csv('./scraped-data/caiso-net-demand.csv')
-        # log the action to download_logs.txt
-        with open(log, 'a') as f: 
-            f.write(date + '\n')
-        # delete this particular file with os.remove()
-        os.remove('./scraped-data/daily/' + file_name)
+    today_df.to_csv('./scraped-data/caiso-net-demand.csv')
+
+    # delete current file from downloads
+    with open(log, 'a') as f: 
+        f.write(date + '\n')
+    os.remove('./scraped-data/daily/' + file_name)
